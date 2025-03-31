bf23855ce50c1d80a10f2fd92e6e2dd696826439
abdullahhauwa <hauwerh35@gmail.com>
2025-03-31 18:17:18 +0100

    wrote APIs

--- backend/database.js
+++ backend/database.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/bootcamp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch(err => {
-    console.error('Error connecting to MongoDB:', err);
-});
+
+// const database = () => {
+//   mongoose.connect('mongodb+srv://Hauwah:KUD917aPCc3i0OPD@mongodemo.8oanb.mongodb.net/?retryWrites=true&w=majority&appName=MongoDemo', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }).then(() => {
+//     console.log('Connected to MongoDB');
+//   }).catch(err => {
+//     console.error('Error connecting to MongoDB:', err);
+//   });
+// }
+
+// export default database;
+
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
-const app = express();
+// import database from './database';
+// const database = require('./database');
+const mongoose = require('mongoose');
 
+
+const database = () => {
+  mongoose.connect('mongodb+srv://Hauwah:KUD917aPCc3i0OPD@mongodemo.8oanb.mongodb.net/?retryWrites=true&w=majority&appName=MongoDemo', {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  }).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+}
+database();
+
+const app = express();
 // Middleware
 app.use(express.json());
 
@@ -9,7 +25,71 @@ app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
 
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+// Post: Create User
+app.post('/users', async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email
+  });
+
+  try{
+      await user.save();
+      // res.send(user);
+      res.status(201).json(user); 
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  console.log(user, 'User created');
+});
+
+// Get: Get all users
+app.get('/users', async (req, res) => {
+  try{
+  const users = await User.find();
+  // res.send(users);
+  res.json(users); 
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Put: Update user
+app.put('/users/:id', async (req, res) => {
+  try{
+     const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+     if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+     res.send(user);   
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete: Delete user
+app.delete('/users/:id', async (req, res) => {
+  try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      if (!user) {
+        res.status(400).json("No user found");
+      }  
+  res.send({ message: 'User deleted' });
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
 // Start server
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
+
